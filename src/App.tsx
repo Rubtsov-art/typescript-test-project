093ce565a7f202b2797a6455efeb8b058018d44b
Pavel <pasharub60@gmail.com>
2020-05-19 12:52:25 +0500

    some types

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  // type string ================
+  const hello: string = 'hello';
+
+  //type in arrow function ======
+
+  const greeting =( text: string) => {
+    alert(text)
+  }
+
+  greeting(hello)
+
+  //type of obj ================
+
+  type person = {
+    sayName?: (name: string) => void
+    name: string,
+    age: number,
+    address?: {
+      country: string,
+      city: string
+    }
+  }
+
+  const firstPerson: person = {
+    sayName (name: string) {alert(name)},
+    name: 'Alex',
+    age: 28,
+    address: {
+      country: 'Russia',
+      city: 'Moscow'
+    }
+  }
+
+  const secondPerson: person = {
+    name: 'Alexandra',
+    age: 26,
+  }
+
+
+//===========================================
+
   return (
     <div className="App">
       <header className="App-header">
